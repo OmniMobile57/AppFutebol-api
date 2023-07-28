9c099cf246c7b774796b179984abf82c7e9dbfb4
Guilherme Américo <107080927+GuiAmerico@users.noreply.github.com>
2023-07-28 18:29:41 -0300

    :tada: init

---
+++ src/main/java/br/com/futdodia/FutdodiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.futdodia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutdodiaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FutdodiaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/br/com/futdodia/FutdodiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.futdodia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutdodiaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
